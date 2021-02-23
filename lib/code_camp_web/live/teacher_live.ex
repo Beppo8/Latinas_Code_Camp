eadf6701f60341b6f51d912ac6583c64e2490da2
maryori <msboneu@gmail.com>
2021-02-22 23:59:20 -0500

    styling of about & teacher's pages

--- lib/code_camp_web/live/teacher_live.ex
+++ lib/code_camp_web/live/teacher_live.ex
@@ -35,7 +35,7 @@ defmodule CodeCampWeb.TeacherLive do
 
   def render(assigns) do
     ~L"""
-    <h1>Teachers</h1>
+    <h1 class="pb-8 px-2 text-2xl text-center uppercase font-bold leading-tight text-blue-900">Tus profesores</h1>
     <div id="teachers">
       <div class="sidebar">
         <nav>
@@ -70,8 +70,11 @@ defmodule CodeCampWeb.TeacherLive do
                   </span>
                 </div>
               </div>
-              <h3>Twitter</h3>
-              <div class="repo">
+           <!--   <h3>
+                Twitter
+              </h3> -->
+              <div class="socials">
+              <button class="btn btn-icon btn-twitter mr-2"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"></path></svg></button>
                 @<%= @selected_teacher.twitter %>
               </div>
             </div>
