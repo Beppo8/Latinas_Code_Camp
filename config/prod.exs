816f22935907673fe250efda02a1c46dad197dee
beppo <beppo@localhost.localdomain>
2021-03-08 17:39:10 -0600

    domain config

--- config/prod.exs
+++ config/prod.exs
@@ -14,8 +14,12 @@ config :code_camp, CodeCampWeb.Endpoint,
   http: [port: {:system, "PORT"}],
   server: true,
   secret_key_base: "${SECRET_KEY_BASE}",
-  url: [host: "${APP_NAME}.gigalixirapp.com", port: 443],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  url: [host: "latinascodecamp.com", port: 443],
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  check_origin = [
+    "//latinascodecamp.com",
+    "//www.latinascodecamp.com"
+  ]
 
 # Do not print debug messages in production
 config :logger, level: :info
