2b699e4fbc90c33103c2e9e844e6f2de68ec8e9f
beppo <beppo@localhost.localdomain>
2021-03-10 09:21:15 -0600

    without domain

--- config/prod.exs
+++ config/prod.exs
@@ -14,7 +14,7 @@ config :code_camp, CodeCampWeb.Endpoint,
   http: [port: {:system, "PORT"}],
   server: true,
   secret_key_base: "${SECRET_KEY_BASE}",
-  url: [host: "latinascodecamp.com", port: 443],
+  url: [host: "${APP_NAME}gigalixir.com", port: 443],
   cache_static_manifest: "priv/static/cache_manifest.json",
 
 # Do not print debug messages in production
